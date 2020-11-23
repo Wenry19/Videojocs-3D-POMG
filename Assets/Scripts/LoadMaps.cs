627b63e2dc1a7bb53cc325308af394dcf062c162
Enric Lamarca Ferrés <anriklf47@gmail.com>
2020-11-23 15:39:04 +0100

    init

---
+++ Assets/Scripts/LoadMaps.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text.RegularExpressions;
+
+public class LoadMaps : MonoBehaviour
+{
+    public TextAsset map;
+    public GameObject Wall;
+    public GameObject colliderInferior, colliderSuperior, colliderIzquierdo, colliderDerecho;
+    // Start is called before the first frame update
+    void generarCollidersInferiores(int[,] matrix)
+    {
+        bool lineStarted = false;
+        int startLine = 0;
+        for (int i = 0; i < 11; i++)
+        {
+            for (int j = 0; j < 15; j++)
+            {
+                if (i + 1 < 11 && matrix[i, j] == 1 && matrix[i + 1, j] == 0)
+                {
+                    if (!lineStarted)
+                    {
+                        lineStarted = true;
+                        startLine = j;
+                    }
+                }
+                else
+                {
+                    if (lineStarted)
+                    {
+                        lineStarted = false;
+                        int size = j - startLine;
+                        float mid = (startLine + ((size - 1) / 2.0f));
+                        GameObject obj = (GameObject)Instantiate(colliderInferior, new Vector3(-7.0f + mid, 5.0f - i, 0.0f), Quaternion.identity);
+                        obj.transform.localScale = new Vector3(size, 1, 1);
+                        obj.transform.parent = transform;
+
+                    }
+                }
+            }
+        }
+    }
+    void generarCollidersSuperiores(int[,] matrix)
+    {
+        bool lineStarted = false;
+        int startLine = 0;
+        for (int i = 1; i < 11; i++)
+        {
+            for (int j = 0; j < 15; j++)
+            {
+                if (matrix[i, j] == 1 && matrix[i - 1, j] == 0)
+                {
+                    if (!lineStarted)
+                    {
+                        lineStarted = true;
+                        startLine = j;
+                    }
+                }
+                else
+                {
+                    if (lineStarted)
+                    {
+                        lineStarted = false;
+                        int size = j - startLine;
+                        float mid = (startLine + ((size - 1) / 2.0f));
+                        GameObject obj = (GameObject)Instantiate(colliderSuperior, new Vector3(-7.0f + mid, 5.0f - i, 0.0f), Quaternion.identity);
+                        obj.transform.localScale = new Vector3(size, 1, 1);
+                        obj.transform.parent = transform;
+
+                    }
+                }
+            }
+        }
+    }
+    void generarCollidersDerechos(int[,] matrix)
+    {
+        bool lineStarted = false;
+        int startLine = 0;
+        for (int j = 0; j < 14; j++)
+        {
+            for (int i = 0; i < 11; i++)
+            {
+                if (matrix[i, j] == 1 && matrix[i, j + 1] == 0)
+                {
+                    if (!lineStarted)
+                    {
+                        lineStarted = true;
+                        startLine = i;
+                    }
+                }
+                else
+                {
+                    if (lineStarted)
+                    {
+                        lineStarted = false;
+                        int size = i - startLine;
+                        float mid = (startLine + ((size - 1) / 2.0f));
+                        GameObject obj = (GameObject)Instantiate(colliderDerecho, new Vector3(-7.0f + j, 5.0f - mid, 0.0f), Quaternion.identity);
+                        obj.transform.localScale = new Vector3(1, size, 1);
+                        obj.transform.parent = transform;
+
+                    }
+                }
+            }
+        }
+    }
+    void generarCollidersIzquierdos(int[,] matrix)
+    {
+        bool lineStarted = false;
+        int startLine = 0;
+        for (int j = 1; j < 15; j++)
+        {
+            for (int i = 0; i < 11; i++)
+            {
+                if (matrix[i, j] == 1 && matrix[i, j - 1] == 0)
+                {
+                    if (!lineStarted)
+                    {
+                        lineStarted = true;
+                        startLine = i;
+                    }
+                }
+                else
+                {
+                    if (lineStarted)
+                    {
+                        lineStarted = false;
+                        int size = i - startLine;
+                        float mid = (startLine + ((size - 1) / 2.0f));
+                        GameObject obj = (GameObject)Instantiate(colliderIzquierdo, new Vector3(-7.0f + j, 5.0f - mid, 0.0f), Quaternion.identity);
+                        obj.transform.localScale = new Vector3(1, size, 1);
+                        obj.transform.parent = transform;
+                    }
+                }
+            }
+        }
+    }
+
+    void Start()
+    {
+        string tiles = map.text;
+        string[] lines = Regex.Split(tiles, "\n");
+        int[,] matrix = new int[11, 15];
+
+        for (int i = 0; i < 11; i++)
+        {
+            for (int j = 0; j < 15; j++)
+            {
+                if (lines[i][j] == '1')
+                {
+                    GameObject obj = (GameObject)Instantiate(Wall, new Vector3(-7.0f + j, 5.0f - i, 0.0f), transform.rotation);
+                    obj.transform.parent = transform;
+                    matrix[i, j] = 1;
+                }
+                else
+                {
+                    matrix[i, j] = 0;
+                }
+            }
+        }
+
+        generarCollidersInferiores(matrix);
+        generarCollidersSuperiores(matrix);
+        generarCollidersDerechos(matrix);
+        generarCollidersIzquierdos(matrix);
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerMoves.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoves : MonoBehaviour
+{
+    public float speedX;
+    public float speedY;
+    bool spacePressed;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.drag = 0;
+        rb.angularDrag = 0;
+        rb.AddForce(new Vector3(speedX, speedY, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            float ySpeed = rb.velocity.y;
+
+            rb.velocity = new Vector3(rb.velocity.x, -ySpeed, 0);
+        }
+    }
+}
