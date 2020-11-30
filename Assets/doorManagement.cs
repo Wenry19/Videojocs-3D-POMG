9ab8f385a9e7af978036be646cf462dcd6c1712f
Enric Lamarca Ferrés <anriklf47@gmail.com>
2020-11-30 20:26:46 +0100

    boton funcional, falta ver el bug de transicion camara

--- Assets/doorManagement.cs
+++ Assets/doorManagement.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class doorManagement : MonoBehaviour
 {
     public int num_doors;
-    List<GameObject> doors;
+    public GameObject[] doors;
+    int current_num_doors = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,15 +16,21 @@ public class doorManagement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (num_doors == current_num_doors) {
+            ButtonActivation ba = GetComponentInParent<ButtonActivation>();
+            ba.doors = doors;
+            current_num_doors += 1; //para cortarlo todo
+            //esto antes estaba en el trigger pero no funcionaba pork para que enviase doors a ba tenia que volver a detectar trigger enter
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log(doors);
         bool already_in = false;
-        if (doors.Count < num_doors)
+        if (current_num_doors < num_doors)
         {
-            for (int i = 0; i < doors.Count; i++)
+            for (int i = 0; i < current_num_doors; i++)
             {
                 if (doors[i].GetInstanceID() == other.GetInstanceID())
                 {
@@ -32,14 +39,20 @@ public class doorManagement : MonoBehaviour
             }
             if (!already_in && other.CompareTag("Wall"))
             {
-                doors.Add(other.gameObject);
+                if (current_num_doors == 0)
+                {
+                    doors = new GameObject[] { other.gameObject };
+                    current_num_doors += 1;
+                }
+                else
+                {
+                    List<GameObject> aux_list = new List<GameObject>(doors);
+                    aux_list.Add(other.gameObject);
+                    doors = aux_list.ToArray();
+                    current_num_doors += 1;
+                }
             }
         }
-        else {
-            ButtonActivation ba = GetComponentInParent<ButtonActivation>();
-            ba.doors = doors.ToArray();
-        }
-
     }
    
 }
