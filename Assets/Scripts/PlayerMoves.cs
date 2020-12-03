e71e69194157fb9465cd21689d054db159c8dc27
Enric Lamarca Ferrés <anriklf47@gmail.com>
2020-12-03 18:35:46 +0100

    ropes implementades

--- Assets/Scripts/PlayerMoves.cs
+++ Assets/Scripts/PlayerMoves.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class PlayerMoves : MonoBehaviour
 {
+    public enum state { 
+        INI, ROPE
+    };
+    public state player_state;
     public float speedX;
     public float speedY;
+    public bool horizontal_rope;
     bool spacePressed;
     Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
+        player_state = state.INI;
         rb = GetComponent<Rigidbody>();
         rb.drag = 0;
         rb.angularDrag = 0;
@@ -20,11 +26,29 @@ public class PlayerMoves : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        desatascar();
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (player_state == state.INI)
         {
-            float ySpeed = rb.velocity.y;
-            rb.velocity = new Vector3(rb.velocity.x, -ySpeed, 0);
+            desatascar();
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                float ySpeed = rb.velocity.y;
+                rb.velocity = new Vector3(rb.velocity.x, -ySpeed, 0);
+            }
+        }
+        else if (player_state == state.ROPE) {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (horizontal_rope)
+                {
+                    float xSpeed = rb.velocity.x;
+                    rb.velocity = new Vector3(-xSpeed, 0, 0);
+                }
+                else
+                {
+                    float ySpeed = rb.velocity.y;
+                    rb.velocity = new Vector3(0, -ySpeed, 0);
+                }
+            }
         }
     }
 
@@ -37,5 +61,10 @@ public class PlayerMoves : MonoBehaviour
         else if (xSpeed >= 0 && xSpeed != 3.75) xSpeed = 3.75f;
 
         rb.velocity = new Vector3(xSpeed, ySpeed, 0);
+    }
+
+    public void change_state(string new_state) {
+        if (new_state == "ROPE") player_state = state.ROPE;
+        else if (new_state == "INI") player_state = state.INI;
     }
 }
---
+++ Assets/Scripts/RopesManagement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopesManagement : MonoBehaviour
+{
+    public bool horizontal = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject player = other.gameObject;
+        PlayerMoves pm = player.GetComponent<PlayerMoves>();
+        if (horizontal)
+        {
+            pm.horizontal_rope = true;
+            player.transform.position = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+
+            if (player.GetComponent<Rigidbody>().velocity.x < 0.0f)
+            {
+                player.GetComponent<Rigidbody>().velocity = new Vector3(-5.37f, 0.0f, 0.0f);
+            }
+            else
+            {
+                player.GetComponent<Rigidbody>().velocity = new Vector3(5.37f, 0.0f, 0.0f);
+            }
+
+        }
+        else {
+            pm.horizontal_rope = false;
+            player.transform.position = new Vector3(transform.position.x, player.transform.position.y, player.transform.position.z);
+
+            if (player.GetComponent<Rigidbody>().velocity.y < 0.0f)
+            {
+                player.GetComponent<Rigidbody>().velocity = new Vector3(0.0f, -5.37f, 0.0f);
+            }
+            else
+            {
+                player.GetComponent<Rigidbody>().velocity = new Vector3(0.0f, 5.37f, 0.0f);
+            }
+        }
+        pm.change_state("ROPE");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        GameObject player = other.gameObject;
+        PlayerMoves pm = player.GetComponent<PlayerMoves>();
+        pm.change_state("INI");
+    }
+}
