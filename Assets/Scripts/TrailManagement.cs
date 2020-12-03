af50184e0b4e16aafe53b4042f38d9bda331588c
Enric Lamarca Ferrés <anriklf47@gmail.com>
2020-12-03 21:15:26 +0100

    trails con instancias de game object

---
+++ Assets/Scripts/TrailManagement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailManagement : MonoBehaviour
+{
+    public GameObject Trail;
+    public float iniTimeToSpawn = 0.5f;
+    float timeToSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeToSpawn = iniTimeToSpawn;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeToSpawn -= Time.deltaTime;
+        if (timeToSpawn <= 0) {
+            timeToSpawn = iniTimeToSpawn;
+            Instantiate(Trail, new Vector3(transform.position.x, transform.position.y, transform.position.z), transform.rotation);
+        }
+    }
+}
--- Assets/Scripts/TriggerTrail.cs
+++ Assets/Scripts/TriggerTrail.cs
@@ -4,36 +4,25 @@ using UnityEngine;
 
 public class TriggerTrail : MonoBehaviour
 {
-    Rigidbody rb_player;
-    Vector3 position_player;
-    public float offset = 1.0f;
+    float timer = 1.0f;
+    bool can_kill = false;
     // Start is called before the first frame update
     void Start()
     {
-        rb_player = GetComponentInParent<Rigidbody>();
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        position_player = transform.parent.transform.position;
-        float posX = 0.0f;
-        float posY = 0.0f;
-        if (rb_player.velocity.x > 0) posX = position_player.x - offset;
-        else if (rb_player.velocity.x < 0) posX = position_player.x + offset;
-        if (rb_player.velocity.y > 0) posY = position_player.y - offset;
-        else if (rb_player.velocity.y < 0) posY = position_player.y + offset;
-
-        transform.position = new Vector3(posX, posY, 0.0f);
-
-        //Debug.Log(posX);
-        //Debug.Log(posY);
-
+        timer -= Time.deltaTime;
+        can_kill = timer <= 0;
     }
 
-    private void OnParticleTrigger()
+    private void OnTriggerEnter(Collider other)
     {
-        //transform.parent.gameObject.SetActive(false);
+        if (other.CompareTag("Player") && can_kill) {
+            //other.gameObject.SetActive(false);
+        }
     }
 }
