ee90a3787ea320043e75dda11e11075300d4b645
Enric Lamarca Ferrés <anriklf47@gmail.com>
2020-12-12 23:54:51 +0100

    intentando collisions con raycast

---
+++ Assets/AddRays.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddRays : MonoBehaviour
+{
+    public GameObject ray;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject raysUp = transform.GetChild(6).gameObject;
+        GameObject raysDown = transform.GetChild(7).gameObject;
+        GameObject raysLeft = transform.GetChild(8).gameObject;
+        GameObject raysRight = transform.GetChild(9).gameObject;
+
+        GameObject raysLeftUp = transform.GetChild(10).gameObject;
+        GameObject raysRightUp = transform.GetChild(11).gameObject;
+        GameObject raysLeftDown = transform.GetChild(12).gameObject;
+        GameObject raysRightDown = transform.GetChild(13).gameObject;
+
+        Vector3 iniposup = transform.position + new Vector3(transform.localScale.x/2.0f, transform.localScale.y/2.0f + 0.01f, transform.localScale.z/2.0f);
+        for (int i = 0; i < 16; i++) {
+            for (int j = 0; j < 16; j++) {
+                GameObject go = Instantiate(ray, iniposup - new Vector3 (0.05f * i, 0.0f, 0.05f * j), transform.rotation);
+                go.transform.parent = raysUp.transform;
+            }
+        }
+
+        Vector3 iniposdown = transform.position + new Vector3(transform.localScale.x / 2.0f, -transform.localScale.y / 2.0f - 0.01f, transform.localScale.z / 2.0f);
+        for (int i = 0; i < 16; i++)
+        {
+            for (int j = 0; j < 16; j++)
+            {
+                GameObject go = Instantiate(ray, iniposdown - new Vector3(0.05f * i, 0.0f, 0.05f * j), transform.rotation);
+                go.transform.parent = raysDown.transform;
+            }
+        }
+
+        Vector3 iniposleft = transform.position + new Vector3(-transform.localScale.x / 2.0f - 0.01f, transform.localScale.y / 2.0f, transform.localScale.z / 2.0f);
+        for (int i = 0; i < 16; i++)
+        {
+            for (int j = 0; j < 16; j++)
+            {
+                GameObject go = Instantiate(ray, iniposleft - new Vector3(0.0f, 0.05f * i, 0.05f * j), transform.rotation);
+                go.transform.parent = raysLeft.transform;
+            }
+        }
+
+        Vector3 iniposright = transform.position + new Vector3(transform.localScale.x / 2.0f + 0.01f, transform.localScale.y / 2.0f, transform.localScale.z / 2.0f);
+        for (int i = 0; i < 16; i++)
+        {
+            for (int j = 0; j < 16; j++)
+            {
+                GameObject go = Instantiate(ray, iniposright - new Vector3(0.0f, 0.05f * i, 0.05f * j), transform.rotation);
+                go.transform.parent = raysRight.transform;
+            }
+        }
+
+        //esquinas
+        Vector3 iniposrightup = transform.position + new Vector3(transform.localScale.x / 2.0f + 0.01f, transform.localScale.y / 2.0f + 0.01f, transform.localScale.z / 2.0f);
+        for (int i = 0; i < 16; i++)
+        {
+            GameObject go = Instantiate(ray, iniposrightup - new Vector3(0.0f, 0.0f, 0.05f * i), transform.rotation);
+            go.transform.parent = raysRightUp.transform;
+        }
+
+        Vector3 iniposleftup = transform.position + new Vector3(-transform.localScale.x / 2.0f - 0.01f, transform.localScale.y / 2.0f + 0.01f, transform.localScale.z / 2.0f);
+        for (int i = 0; i < 16; i++)
+        {
+            GameObject go = Instantiate(ray, iniposleftup - new Vector3(0.0f, 0.0f, 0.05f * i), transform.rotation);
+            go.transform.parent = raysLeftUp.transform;
+        }
+
+        Vector3 iniposrightDown = transform.position + new Vector3(transform.localScale.x / 2.0f + 0.01f, -transform.localScale.y / 2.0f - 0.01f, transform.localScale.z / 2.0f);
+        for (int i = 0; i < 16; i++)
+        {
+            GameObject go = Instantiate(ray, iniposrightDown - new Vector3(0.0f, 0.0f, 0.05f * i), transform.rotation);
+            go.transform.parent = raysRightDown.transform;
+        }
+
+        Vector3 iniposleftDown = transform.position + new Vector3(-transform.localScale.x / 2.0f - 0.01f, -transform.localScale.y / 2.0f - 0.01f, transform.localScale.z / 2.0f);
+        for (int i = 0; i < 16; i++)
+        {
+            GameObject go = Instantiate(ray, iniposleftDown - new Vector3(0.0f, 0.0f, 0.05f * i), transform.rotation);
+            go.transform.parent = raysLeftDown.transform;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/RaysManage.cs
+++ Assets/RaysManage.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class RaysManage : MonoBehaviour
 {
     List<Ray> raysUp;
-    List <Ray> raysDown;
-    List <Ray> raysLeft;
-    List <Ray> raysRight;
+    List<Ray> raysDown;
+    List<Ray> raysLeft;
+    List<Ray> raysRight;
+
+    List<Ray> raysLeftUp;
+    List<Ray> raysRightUp;
+    List<Ray> raysLeftDown;
+    List<Ray> raysRightDown;
 
     RaycastHit hit;
     PlayerMoves pm;
 
     public float hit_dist;
+    public float hit_dist_esq;
 
     // Start is called before the first frame update
     void Start()
@@ -24,11 +30,26 @@ public class RaysManage : MonoBehaviour
         raysDown = new List<Ray>();
         raysLeft = new List<Ray>();
         raysRight = new List<Ray>();
+        raysLeftUp = new List<Ray>();
+        raysRightUp = new List<Ray>();
+        raysLeftDown = new List<Ray>();
+        raysRightDown = new List<Ray>();
 
-        raysUp.Add(new Ray(transform.GetChild(6).transform.GetChild(0).transform.position, Vector3.up));
-        raysDown.Add(new Ray(transform.GetChild(7).transform.GetChild(0).transform.position, Vector3.down));
-        raysLeft.Add(new Ray(transform.GetChild(8).transform.GetChild(0).transform.position, Vector3.left));
-        raysRight.Add(new Ray(transform.GetChild(9).transform.GetChild(0).transform.position, Vector3.right));
+        for (int i = 0; i < transform.GetChild(6).childCount; i++)
+        {
+            raysUp.Add(new Ray(transform.GetChild(6).transform.GetChild(i).transform.position, Vector3.up));
+            raysDown.Add(new Ray(transform.GetChild(7).transform.GetChild(i).transform.position, Vector3.down));
+            raysLeft.Add(new Ray(transform.GetChild(8).transform.GetChild(i).transform.position, Vector3.left));
+            raysRight.Add(new Ray(transform.GetChild(9).transform.GetChild(i).transform.position, Vector3.right));
+        }
+
+        for (int i = 0; i < transform.GetChild(10).childCount; i++)
+        {
+            raysLeftUp.Add(new Ray(transform.GetChild(10).transform.GetChild(i).transform.position, Vector3.left + Vector3.up));
+            raysRightUp.Add(new Ray(transform.GetChild(11).transform.GetChild(i).transform.position, Vector3.right + Vector3.up));
+            raysLeftDown.Add(new Ray(transform.GetChild(12).transform.GetChild(i).transform.position, Vector3.left + Vector3.down));
+            raysRightDown.Add(new Ray(transform.GetChild(13).transform.GetChild(i).transform.position, Vector3.right + Vector3.down));
+        }
     }
 
     // Update is called once per frame
@@ -45,12 +66,12 @@ public class RaysManage : MonoBehaviour
         for (int i = 0; i < raysUp.Count; i++)
         {
             Debug.DrawRay(transform.GetChild(6).transform.GetChild(0).transform.position, Vector3.up, Color.black);
-            raysUp[i] = new Ray(transform.GetChild(6).transform.GetChild(0).transform.position, Vector3.up);
+            raysUp[i] = new Ray(transform.GetChild(6).transform.GetChild(i).transform.position, Vector3.up);
             if (Physics.Raycast(raysUp[i], out hit))
             {
                 //print("up_dist: " + hit.distance);
                 if ((Mathf.Abs(hit.distance) < hit_dist) && (hit.collider.CompareTag("Wall") || (hit.collider.CompareTag("Door") && hit.collider.name[0] != 'C' && hit.collider.gameObject.GetComponent<Renderer>().enabled) ||
-                    hit.collider.CompareTag("Switch") || (hit.collider.CompareTag("InviDoor") && hit.collider.name[0] != 'C' && hit.collider.gameObject.GetComponent<Renderer>().enabled) || 
+                    hit.collider.CompareTag("Switch") || (hit.collider.CompareTag("InviDoor") && hit.collider.name[0] != 'C' && hit.collider.gameObject.GetComponent<Renderer>().enabled) ||
                     (hit.collider.CompareTag("TrailDoor") && !pm.isTrailing())))
                 {
                     //print("up");
@@ -66,7 +87,7 @@ public class RaysManage : MonoBehaviour
         for (int i = 0; i < raysDown.Count; i++)
         {
             Debug.DrawRay(transform.GetChild(7).transform.GetChild(0).transform.position, Vector3.down, Color.red);
-            raysDown[i] = new Ray(transform.GetChild(7).transform.GetChild(0).transform.position, Vector3.down);
+            raysDown[i] = new Ray(transform.GetChild(7).transform.GetChild(i).transform.position, Vector3.down);
             if (Physics.Raycast(raysDown[i], out hit))
             {
                 //print("down_dist: " + hit.distance);
@@ -86,7 +107,7 @@ public class RaysManage : MonoBehaviour
         for (int i = 0; i < raysRight.Count; i++)
         {
             Debug.DrawRay(transform.GetChild(9).transform.GetChild(0).transform.position, Vector3.right, Color.green);
-            raysRight[i] = new Ray(transform.GetChild(9).transform.GetChild(0).transform.position, Vector3.right);
+            raysRight[i] = new Ray(transform.GetChild(9).transform.GetChild(i).transform.position, Vector3.right);
             if (Physics.Raycast(raysRight[i], out hit))
             {
                 //print("right_dist: " + hit.distance);
@@ -107,7 +128,7 @@ public class RaysManage : MonoBehaviour
         for (int i = 0; i < raysLeft.Count; i++)
         {
             Debug.DrawRay(transform.GetChild(8).transform.GetChild(0).transform.position, Vector3.left, Color.blue);
-            raysLeft[i] = new Ray(transform.GetChild(8).transform.GetChild(0).transform.position, Vector3.left);
+            raysLeft[i] = new Ray(transform.GetChild(8).transform.GetChild(i).transform.position, Vector3.left);
             if (Physics.Raycast(raysLeft[i], out hit))
             {
                 //print("left_dist: " + hit.distance);
@@ -122,5 +143,93 @@ public class RaysManage : MonoBehaviour
             }
         }
     }
+
+//esquinas
+void checkCollLeftUp()
+{
+    for (int i = 0; i < raysLeftUp.Count; i++)
+    {
+        Debug.DrawRay(transform.GetChild(10).transform.GetChild(i).transform.position, Vector3.left + Vector3.up, Color.blue);
+        raysLeftUp[i] = new Ray(transform.GetChild(10).transform.GetChild(i).transform.position, Vector3.left + Vector3.up);
+        if (Physics.Raycast(raysLeftUp[i], out hit))
+        {
+            //print("left_dist: " + hit.distance);
+            if ((Mathf.Abs(hit.distance) < hit_dist_esq) && (hit.collider.CompareTag("Wall") || (hit.collider.CompareTag("Door") && hit.collider.name[0] != 'C' && hit.collider.gameObject.GetComponent<Renderer>().enabled) ||
+                hit.collider.CompareTag("Switch") || (hit.collider.CompareTag("InviDoor") && hit.collider.name[0] != 'C' && hit.collider.gameObject.GetComponent<Renderer>().enabled) ||
+                (hit.collider.CompareTag("TrailDoor") && !pm.isTrailing())))
+            {
+                //print("left");
+                pm.collis(0);
+                pm.collis(3);
+                break;
+            }
+        }
+    }
+}
+void checkCollRightUp()
+{
+    for (int i = 0; i < raysRightUp.Count; i++)
+    {
+        Debug.DrawRay(transform.GetChild(11).transform.GetChild(i).transform.position, Vector3.right + Vector3.up, Color.blue);
+        raysRightUp[i] = new Ray(transform.GetChild(11).transform.GetChild(i).transform.position, Vector3.right + Vector3.up);
+        if (Physics.Raycast(raysRightUp[i], out hit))
+        {
+            //print("left_dist: " + hit.distance);
+            if ((Mathf.Abs(hit.distance) < hit_dist_esq) && (hit.collider.CompareTag("Wall") || (hit.collider.CompareTag("Door") && hit.collider.name[0] != 'C' && hit.collider.gameObject.GetComponent<Renderer>().enabled) ||
+                hit.collider.CompareTag("Switch") || (hit.collider.CompareTag("InviDoor") && hit.collider.name[0] != 'C' && hit.collider.gameObject.GetComponent<Renderer>().enabled) ||
+                (hit.collider.CompareTag("TrailDoor") && !pm.isTrailing())))
+            {
+                //print("left");
+                pm.collis(0);
+                pm.collis(2);
+                break;
+            }
+        }
+    }
+}
+
+void checkCollLeftDown()
+{
+    for (int i = 0; i < raysLeftDown.Count; i++)
+    {
+        Debug.DrawRay(transform.GetChild(12).transform.GetChild(i).transform.position, Vector3.left + Vector3.down, Color.blue);
+        raysLeftDown[i] = new Ray(transform.GetChild(12).transform.GetChild(i).transform.position, Vector3.left + Vector3.down);
+        if (Physics.Raycast(raysLeftDown[i], out hit))
+        {
+            //print("left_dist: " + hit.distance);
+            if ((Mathf.Abs(hit.distance) < hit_dist_esq) && (hit.collider.CompareTag("Wall") || (hit.collider.CompareTag("Door") && hit.collider.name[0] != 'C' && hit.collider.gameObject.GetComponent<Renderer>().enabled) ||
+                hit.collider.CompareTag("Switch") || (hit.collider.CompareTag("InviDoor") && hit.collider.name[0] != 'C' && hit.collider.gameObject.GetComponent<Renderer>().enabled) ||
+                (hit.collider.CompareTag("TrailDoor") && !pm.isTrailing())))
+            {
+                    //print("left");
+                pm.collis(1);
+                pm.collis(3);
+                break;
+            }
+        }
+    }
+}
+
+void checkCollRightDown()
+{
+    for (int i = 0; i < raysRightDown.Count; i++)
+    {
+        Debug.DrawRay(transform.GetChild(13).transform.GetChild(i).transform.position, Vector3.right + Vector3.down, Color.blue);
+        raysRightDown[i] = new Ray(transform.GetChild(13).transform.GetChild(i).transform.position, Vector3.right + Vector3.down);
+        if (Physics.Raycast(raysRightDown[i], out hit))
+        {
+            //print("left_dist: " + hit.distance);
+            if ((Mathf.Abs(hit.distance) < hit_dist_esq) && (hit.collider.CompareTag("Wall") || (hit.collider.CompareTag("Door") && hit.collider.name[0] != 'C' && hit.collider.gameObject.GetComponent<Renderer>().enabled) ||
+                hit.collider.CompareTag("Switch") || (hit.collider.CompareTag("InviDoor") && hit.collider.name[0] != 'C' && hit.collider.gameObject.GetComponent<Renderer>().enabled) ||
+                (hit.collider.CompareTag("TrailDoor") && !pm.isTrailing())))
+            {
+                //print("left");
+                pm.collis(1);
+                pm.collis(2);
+                break;
+            }
+        }
+    }
+}
 
 }
