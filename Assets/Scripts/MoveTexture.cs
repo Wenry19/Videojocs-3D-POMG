ec42ba66de61464d87533abcf972247b1434047b
Arian Huapaya <55849001+ArianH23@users.noreply.github.com>
2020-12-15 05:56:19 +0100

    Diseño level 4 en progreso

---
+++ Assets/Scripts/MoveTexture.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTexture : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Vector2 v = new Vector2(0, 0);
+    public float speedX = 2;
+    public float speedY = 2;
+    Renderer rend;
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        v.x += speedX * Time.deltaTime;
+        v.y += speedY * Time.deltaTime;
+
+        rend.material.SetTextureOffset("_MainTex", v);
+
+    }
+}
